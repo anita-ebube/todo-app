5126203af04a6ceffd9d6ce19473e5830ce4bcd4
anita-ebube <oluebubechukwuanita@gmail.com>
2024-09-19 22:32:18 +0100

    Made update to the style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { TodoWrapper } from "./components/TodoWrapper/page";
 function App() {
   return (
     <div className="bg-[#f5f5f5] text-white h-screen flex justify-center items-center ">
-      <div className="bg-black p-5 m-5 rounded container">
+      <div className="bg-black lg:500px p-5 m-5 rounded container">
         <TodoWrapper />
         
       </div>
